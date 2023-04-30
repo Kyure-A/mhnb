a5ad966da5ab56d65031f0111b573c2e182bbd15
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-04-30 16:48:51 +0900

    [Fixed] .env

--- glitch/src/deploy.ts
+++ glitch/src/deploy.ts
@@ -2,6 +2,7 @@ import { SlashCommandBuilder, REST, Routes } from "discord.js";
 import { APIUser } from "discord-api-types/v10"
 import fs from "node:fs";
 import config from "../.config.json"
+require("dotenv").config();
 
 const commands: any[] = [];
 const files = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -13,7 +14,7 @@ for (const file of files) {
 
 //登録用関数
 
-const rest = new REST({ version: '10' }).setToken(config.token)
+const rest = new REST({ version: '10' }).setToken(process.env.token!)
 async function main() {
     await rest.put(
         Routes.applicationCommands("BOTのユーザーIDをコピーして貼り付ける"),
