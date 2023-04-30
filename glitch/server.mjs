04941c55003a238bc8dcebde740fd42c2c653382
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-04-30 17:50:56 +0900

    [Modified] server.mts

--- glitch/server.mjs
+++ glitch/server.mjs
@@ -4,7 +4,7 @@ import { Client, Events, GatewayIntentBits } from "discord.js";
 // const create = require("commands/create")
 // const list = require("commands/list")
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.once(Events.ClientReady, c => {
-    console.log(`$Logined {c.user.tag}`);
+client.once(Events.ClientReady, () => {
+    console.log("Ready");
 });
 client.login(process.env.token);
