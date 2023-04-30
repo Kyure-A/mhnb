70fab64b3e67fd8533cba256cf2340c240a2c53f
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-04-30 16:24:39 +0900

    [fixed] bug fixed

--- glitch/src/deploy.ts
+++ glitch/src/deploy.ts
@@ -1,10 +1,9 @@
-// コマンド設定部分
-const { SlashCommandBuilder } = require("discord.js");
-const { APIUser } = require('discord-api-types/v10');
-const { REST, Routes } = require("discord.js");
-const fs = require('node:fs');
+import { SlashCommandBuilder, REST, Routes } from "discord.js";
+import { APIUser } from "discord-api-types/v10"
+import fs from "node:fs";
+import config from "../.config.json"
 
-const commands = [];
+const commands: any[] = [];
 const files = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of files) {
@@ -14,7 +13,7 @@ for (const file of files) {
 
 //登録用関数
 
-const rest = new REST({ version: '10' }).setToken(process.env.token)
+const rest = new REST({ version: '10' }).setToken(config.token)
 async function main() {
     await rest.put(
         Routes.applicationCommands("BOTのユーザーIDをコピーして貼り付ける"),
