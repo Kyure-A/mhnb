f39fdfb98824ff218ba2e7ed6b5a9432a05561fe
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-04-30 16:57:42 +0900

    [Fixed] conflict

--- glitch/src/server.ts
+++ glitch/src/server.ts
@@ -1,9 +1,5 @@
 import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
-<<<<<<< HEAD
 require("dotenv").config();
-=======
-import { application_id, guild_id, token } from "../.config.json"
->>>>>>> a12db89e35ba6182b4c8baa249785316e4f48410
 
 const create = require("commands/create")
 const list = require("commands/list")
@@ -13,8 +9,6 @@ client.once(Events.ClientReady, c => {
     console.log(`$Logined {c.user.tag}`);
 });
 
-<<<<<<< HEAD
 client.login(process.env.token!);
-=======
-client.login(token);
->>>>>>> a12db89e35ba6182b4c8baa249785316e4f48410
+
+
