83bdc6e09e21325d6ab67cceeb866dea67cd95d7
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-05-06 20:34:21 +0900

    [Modified] modulize

--- gas/src/index.ts
+++ gas/src/index.ts
@@ -1,22 +1,3 @@
-function wakeGlitch(): void {
-    const glitch_url: string | null = PropertiesService.getDocumentProperties().getProperty("glitch_url");
-    const data = {};
-    const headers = { 'Content-Type': 'application/json; charset=UTF-8' };
-    const params: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
-        "method": "post",
-        "payload": JSON.stringify(data),
-        "headers": headers,
-    }
-    const response = UrlFetchApp.fetch(glitch_url!, params);
-    console.log(response);
-}
+import { wakeGlitch } from "modules"
 
-// スプレッドシートの内容を JSON で返す
-function doGet() {
-    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
-}
-
-// スプレッドシートに内容を追記するまたは内容を削除する
-function doPost(e) {
-
-}
+(global as any).wakeGlitch = wakeGlitch
---
+++ gas/src/modules/index.ts
@@ -0,0 +1,22 @@
+export function wakeGlitch(): void {
+    const glitch_url: string | null = PropertiesService.getDocumentProperties().getProperty("glitch_url");
+    const data = {};
+    const headers = { 'Content-Type': 'application/json; charset=UTF-8' };
+    const params: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+        "method": "post",
+        "payload": JSON.stringify(data),
+        "headers": headers,
+    }
+    const response = UrlFetchApp.fetch(glitch_url!, params);
+    console.log(response);
+}
+
+// スプレッドシートの内容を JSON で返す
+export function doGet() {
+    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
+}
+
+// スプレッドシートに内容を追記するまたは内容を削除する
+export function doPost(e: any) {
+
+}
