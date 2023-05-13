8dd71faf498e614494f8cdc41f51676f632cca4d
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-05-13 17:52:06 +0900

    [Modified] date で sort するようにした

--- gas/src/modules/index.ts
+++ gas/src/modules/index.ts
@@ -1,3 +1,5 @@
+import { parse } from "date-fns"
+
 export function wakeGlitch(): void {
     const glitch_url: string | null = PropertiesService.getDocumentProperties().getProperty("glitch_url");
     const data = {};
@@ -11,57 +13,33 @@ export function wakeGlitch(): void {
     console.log(response);
 }
 
+export async function sortSheet(sheet: GoogleAppsScript.Spreadsheet.Sheet): Promise<void> {
+    const range = sheet.getRange(1, 1, sheet!.getLastRow(), sheet!.getLastColumn());
+    range.sort({ column: 3, ascending: true });
+    range.removeDuplicates();
+}
+
 export function taskBuilder(value: any[][]): Field[] {
-    let date_set: Set<number> = new Set<number>();
-    let task_map: Map<number, Field[]> = new Map<number, Field[]>();
-    const now: Date = new Date();
+    let fields: Field[] = [];
 
     for (let i = 0; i < value.length; i++) {
         const homework_name: string = value[i][0];
         const subject_name: string = value[i][1];
-        const month_str: string = value[i][2];
-        const day_str: string = value[i][3];
-        const description: string = value[i][4];
+        const date_str: string = value[i][2];
+        const description: string = value[i][3];
 
-        const month: number = parseInt(month_str);
-        const day: number = parseInt(day_str);
+        const date: Date = parse(date_str, "yyyy/MM/dd", new Date());
 
-        const task: Field = {
+        const month: number = date.getMonth() + 1;
+        const day: number = date.getDate();
+
+        const field: Field = {
             "name": `${subject_name}: ${homework_name} (${month}/${day})`,
             "value": description,
             "inline": false
         }
 
-        let adder: 0 | 1 = 0;
-        if (now.getMonth() > month) adder = 1; // その年度の課題しか想定していないので，1 年またぐ課題とかは無理
-
-        const task_date: number = parseInt(Utilities.formatDate(new Date(now.getFullYear() + adder, month, day), "JST", "yyMMdd"));
-        date_set.add(task_date);
-
-        if (task_map.has(task_date) && task_map !== undefined) task_map.get(task_date)!.push(task);
-        else {
-            task_map.set(task_date, []);
-            task_map.get(task_date)!.push(task);
-        }
-    }
-
-    let date_array: number[] = Array.from(date_set).sort();
-
-    let fields: Field[] = [];
-
-    let counter: number = 1;
-    for (let i = 0; i < date_array.length; i++) {
-        const task_array: Field[] | undefined = task_map.get(date_array[i]);
-
-        if (task_array !== undefined) {
-
-            for (let j = 0; j < task_array.length; j++) {
-                let task = task_array[j];
-                task.name = `[${counter}] ` + task.name;
-                fields.push(task);
-                counter++;
-            }
-        }
+        fields.push(field);
     }
 
     return fields;
@@ -70,11 +48,23 @@ export function taskBuilder(value: any[][]): Field[] {
 export async function doCreate(sheet: GoogleAppsScript.Spreadsheet.Sheet, params: any): Promise<void> {
     const homework: string = await params.homework;
     const subject: string = await params.subject;
-    const month: string = await params.month;
-    const day: string = await params.day;
+    const month_str: string = await params.month;
+    const day_str: string = await params.day;
     const description: string = await params.description;
 
-    sheet.appendRow([homework, subject, month, day, description]);
+    const month = parseInt(month_str);
+    const day = parseInt(day_str);
+
+    const now: Date = new Date();
+
+    let adder: 0 | 1 = 0;
+    if (now.getMonth() > month) adder = 1;
+
+    const date: Date = new Date(now.getFullYear() + adder, month - 1, day);
+
+    sheet.appendRow([homework, subject, date, description]);
+
+    await sortSheet(sheet);
 }
 
 export function doDelete(sheet: GoogleAppsScript.Spreadsheet.Sheet) {
