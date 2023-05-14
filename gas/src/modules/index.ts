08f2a971593a4a2922c395edb267571ab6a411a2
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2023-05-14 18:15:17 +0900

    [Added] /edit command

--- gas/src/modules/index.ts
+++ gas/src/modules/index.ts
@@ -19,7 +19,7 @@ export function sortSheet(sheet: GoogleAppsScript.Spreadsheet.Sheet): void {
     range.removeDuplicates();
 }
 
-export function taskBuilder(sheet: GoogleAppsScript.Spreadsheet.Sheet): Field[] {
+export function fieldsBuilder(sheet: GoogleAppsScript.Spreadsheet.Sheet): Field[] {
     const value: string[][] = sheet!.getRange(1, 1, sheet!.getLastRow(), sheet!.getLastColumn()).getDisplayValues();
 
     sortSheet(sheet);
@@ -76,6 +76,11 @@ export async function doCreate(sheet: GoogleAppsScript.Spreadsheet.Sheet, params
     sortSheet(sheet);
 }
 
+export function doList(sheet: GoogleAppsScript.Spreadsheet.Sheet): GoogleAppsScript.Content.TextOutput {
+    const fields: Field[] = fieldsBuilder(sheet!);
+    return ContentService.createTextOutput(JSON.stringify(fields)).setMimeType(ContentService.MimeType.JSON);
+}
+
 export async function doDelete(sheet: GoogleAppsScript.Spreadsheet.Sheet, params: any) {
     const task_number: number = await params.task_number;
 
@@ -84,13 +89,70 @@ export async function doDelete(sheet: GoogleAppsScript.Spreadsheet.Sheet, params
     sortSheet(sheet);
 }
 
-// "/list"
-export function doGet(): GoogleAppsScript.Content.TextOutput {
+export function doGetEdit(sheet: GoogleAppsScript.Spreadsheet.Sheet, params: any): GoogleAppsScript.Content.TextOutput {
+    const i: number = params.task_number;
+    const value: string[][] = sheet!.getRange(1, 1, sheet!.getLastRow(), sheet!.getLastColumn()).getDisplayValues();
+    const homework_name: string = value[i][0];
+    const subject_name: string = value[i][1];
+    const date_str: string = value[i][2];
+    const description: string = value[i][3];
+
+    const date: Date = parse(date_str, "yyyy/MM/dd", new Date());
+
+    const month: string = String(date.getMonth() + 1);
+    const day: string = String(date.getDate());
+
+    const json = {
+        "homework": homework_name,
+        "subject": subject_name,
+        "month": month,
+        "day": day,
+        "description": description
+    }
+
+    return ContentService.createTextOutput(JSON.stringify(json)).setMimeType(ContentService.MimeType.JSON);
+}
+
+export async function doPostEdit(sheet: GoogleAppsScript.Spreadsheet.Sheet, params: any) {
+    const task_str: string = await params.task_number;
+    const homework: string = await params.homework;
+    const subject: string = await params.subject;
+    const month_str: string = await params.month;
+    const day_str: string = await params.day;
+    const description: string = await params.description;
+
+    const task_number: number = parseInt(task_str);
+    const month = parseInt(month_str);
+    const day = parseInt(day_str);
+
+    const now: Date = new Date();
+
+    let adder: 0 | 1 = 0;
+    if (now.getMonth() > month) adder = 1;
+
+    const date: Date = new Date(now.getFullYear() + adder, month - 1, day);
+
+    sheet.getRange(task_number, 1).setValue(homework);
+    sheet.getRange(task_number, 2).setValue(subject);
+    sheet.getRange(task_number, 3).setValue(date);
+    sheet.getRange(task_number, 4).setValue(description);
+}
+
+// "/list", "/edit"
+export function doGet(e: any): GoogleAppsScript.Content.TextOutput {
     const sheet: GoogleAppsScript.Spreadsheet.Sheet | null = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("data");
+    const params = JSON.parse(e.postData.getDataAsString());
+    const get_type: string = params.command;
 
-    const fields: Field[] = taskBuilder(sheet!);
+    if (get_type == "edit") {
+        const output: GoogleAppsScript.Content.TextOutput = doGetEdit(sheet!, params);
+        return output;
+    }
 
-    return ContentService.createTextOutput(JSON.stringify(fields)).setMimeType(ContentService.MimeType.JSON);
+    else { // get_type == "list"
+        const output: GoogleAppsScript.Content.TextOutput = doList(sheet!);
+        return output;
+    }
 }
 
 // "/create", "/delete"
@@ -109,7 +171,7 @@ export async function doPost(e: any): Promise<GoogleAppsScript.Content.TextOutpu
     }
 
     if (post_type == "edit") {
-
+        await doPostEdit(sheet!, params);
     }
 
     const output = `/${post_type} による POST が受け取られました！`
--- glitch/src/commands/edit.ts
+++ glitch/src/commands/edit.ts
@@ -4,11 +4,107 @@ import axios, { isCancel, AxiosError } from "axios";
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('edit')
-        .setDescription('指定した課題の内容を編集します'),
+        .setDescription('指定した課題の内容を編集します')
+        .addIntegerOption(option =>
+            option.setName("task_number")
+                .setDescription("課題の番号")
+                .setRequired(true)),
+
     execute: async function(interaction: any) {
+        if (!interaction.isChatInputCommand()) return;
+
+        if (interaction.commandName == "edit") {
+
+            const task_number: number = await interaction.options.getInteger("task_number")
+
+            await axios.get(process.env.gas_url!)
+                .then(response => {
+                    const json = response.data;
+
+                    const modal: ModalBuilder = new ModalBuilder()
+                        .setCustomId("edit")
+                        .setTitle("課題入力フォーム");
+                    const homework_name: TextInputBuilder = new TextInputBuilder()
+                        .setCustomId("homework_name")
+                        .setLabel("課題名")
+                        .setMinLength(1)
+                        .setMaxLength(30)
+                        .setStyle(TextInputStyle.Short)
+                        .setValue(json.homework);
+                    const subject_name: TextInputBuilder = new TextInputBuilder()
+                        .setCustomId("subject_name")
+                        .setLabel("教科名")
+                        .setPlaceholder("シラバスに載っている正式名称で入力してください")
+                        .setMinLength(1)
+                        .setMaxLength(20)
+                        .setStyle(TextInputStyle.Short)
+                        .setValue(json.subject);
+                    const month: TextInputBuilder = new TextInputBuilder()
+                        .setCustomId("month")
+                        .setLabel("期限 (月)")
+                        .setPlaceholder("月を 1 - 12 で入力してください")
+                        .setMinLength(1)
+                        .setMaxLength(2)
+                        .setStyle(TextInputStyle.Short)
+                        .setValue(json.month);
+                    const day: TextInputBuilder = new TextInputBuilder()
+                        .setCustomId("day")
+                        .setLabel("期限 (日)")
+                        .setPlaceholder("日を 1 - 31 で入力してください")
+                        .setMinLength(1)
+                        .setMaxLength(2)
+                        .setStyle(TextInputStyle.Short)
+                        .setValue(json.day);
+                    const description: TextInputBuilder = new TextInputBuilder()
+                        .setCustomId("description")
+                        .setLabel("説明")
+                        .setPlaceholder("")
+                        .setMinLength(1)
+                        .setMaxLength(100)
+                        .setStyle(TextInputStyle.Paragraph)
+                        .setValue(json.description);
+
+                    modal.addComponents(
+                        new ActionRowBuilder<TextInputBuilder>().addComponents(homework_name),
+                        new ActionRowBuilder<TextInputBuilder>().addComponents(subject_name),
+                        new ActionRowBuilder<TextInputBuilder>().addComponents(month),
+                        new ActionRowBuilder<TextInputBuilder>().addComponents(day),
+                        new ActionRowBuilder<TextInputBuilder>().addComponents(description),
+                    )
+
+                    interaction.showModal(modal);
+                })
+                .catch(error => {
+                    console.error(error);
+                    interaction.editReply("axios in edit.js threw error")
+                });
+        }
 
     },
     modal: async function(interaction: any) {
+        const homework: string = await interaction.fields.getTextInputValue("homework_name");
+        const subject: string = await interaction.fields.getTextInputValue("subject_name");
+        const month: string = await interaction.fields.getTextInputValue("month");
+        const day: string = await interaction.fields.getTextInputValue("day");
+        const description: string = await interaction.fields.getTextInputValue("description");
 
+        const json = {
+            "command": "edit",
+            "homework": homework,
+            "subject": subject,
+            "month": month,
+            "day": day,
+            "description": description
+        }
+
+        await axios.post(process.env.gas_url!, json)
+            .then(response => {
+                console.log(response.data);
+                interaction.reply(`課題「${homework}」が編集されました！`);
+                console.log("Modal is send.")
+            })
+            .catch(error => {
+                console.error(error);
+            })
     }
 }
